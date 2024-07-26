089350310ede1dfdbf611fba119f687df7ae35e3
akhilprabhu20 <117083761+akhilprabhu20@users.noreply.github.com>
2024-07-26 13:31:12 +0530

    Delete day-14/start_container.sh

--- day-14/start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Start the running container (if any)
-docker pull akhilprabhu2005/simple-python-flask-app
-
-#run the docker-image as container
-
-docker run -d -p 5000:5000 akhilprabhu2005/simple-python-flask-app
